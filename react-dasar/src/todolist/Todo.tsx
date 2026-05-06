3a35e53a8e17ed569a82ed1fb494e6c05919aee4
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-06 20:33:25 +0700

    Logical AND

--- react-dasar/src/todolist/Todo.tsx
+++ react-dasar/src/todolist/Todo.tsx
@@ -5,7 +5,13 @@ export function Todo({ isCompleted, text, isDeleted }:
     } else {
         return (
             <li>
-                {isCompleted ? <del>{text}</del> : text}
+                {/* Ternary operator cara 1 */}
+                {/* {isCompleted ? <del>{text}</del> : text} */}
+
+                {/* Ternary operator cara 2 
+                Tampilkan teks, lalu jika sudah selesai (isCompleted), tampilkan juga tanda centang ✅. Jika belum selesai, jangan tampilkan apa-apa setelah teks
+                */}
+                {text} {isCompleted && '✅'}
             </li>
         )
     }
