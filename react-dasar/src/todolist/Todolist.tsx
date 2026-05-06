33c8f34f04ba862ea6420ba30d4690284231c504
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-06 20:47:00 +0700

    Collection Component

--- react-dasar/src/todolist/Todolist.tsx
+++ react-dasar/src/todolist/Todolist.tsx
@@ -1,11 +1,40 @@
 import { Todo } from "./Todo";
 
+type TodoItem = {
+    text: string;
+    isCompleted: boolean;
+}
+
 export default function Todolist() {
+
+    const data: TodoItem[] = [
+        {
+            text: 'Belajar HTML',
+            isCompleted: true
+        },
+        {
+            text: 'Belajar Typescript',
+            isCompleted: true
+        },
+        {
+            text: 'Belajar React',
+            isCompleted: false
+        }
+    ]
+
+    const todos = data.map((item) => <Todo {...item} />)
+
     return (
+        //Tanpa loop 
+        // <ul>
+        //     <Todo isCompleted={true} text="Belajar React" isDeleted={true} />
+        //     <Todo isCompleted={true} text="Belajar Vue" />
+        //     <Todo isCompleted={false} text="Belajar Laravel" />
+        // </ul>
+
+        //Dengan collection component
         <ul>
-            <Todo isCompleted={true} text="Belajar React" isDeleted={true} />
-            <Todo isCompleted={true} text="Belajar Vue" />
-            <Todo isCompleted={false} text="Belajar Laravel" />
+            { todos }
         </ul>
     )
 }
