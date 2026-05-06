73f80d99d5ad635279389be9b1d5571ac9cb2e02
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-06 22:27:27 +0700

    Event Object

--- react-dasar/src/button/AlertButton.tsx
+++ react-dasar/src/button/AlertButton.tsx
@@ -1,11 +1,15 @@
+import type { MouseEvent } from "react";
+
 export default function AlertButton({ text, message }: { text: string, message: string }) {
-    function handleClick() {
+    function handleClick(event: MouseEvent<HTMLButtonElement>): void {
+        console.log(event)
+        console.log(event.target)
         alert(message);
     }
     return (
         <>
             {/* Cara 1 */}
-            <button onClick={handleClick}>{text}</button>
+            <button onClick={handleClick}>Event button</button>
 
             {/* Cara 2 */}
             <button onClick={() => alert(message)}>{text}</button>
