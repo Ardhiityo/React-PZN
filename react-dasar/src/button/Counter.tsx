a1196f05040aa1456c28578523da1c205f9de34f
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-07 13:08:24 +0700

    Snapshot

--- react-dasar/src/button/Counter.tsx
+++ react-dasar/src/button/Counter.tsx
@@ -3,8 +3,21 @@ import { useState } from "react";
 export default function Counter() {
     const [count, setCount] = useState(0);
 
+    //proses: click > render ulang > state berubah
+    
+    //akan bernilai 1 ketika proses render selesai
+    console.log(`Trigger count ${count}`)
+    
+    //pada saat diklik count masih bernilai 0, ketika proses render selesai, maka count bernilai 1
     function handleClick() {
         setCount(count + 1);
+        
+        //meskipun set count 3x maka tetap saja increment dilakukan +1
+        // setCount(count + 1);
+        // setCount(count + 1);
+        // setCount(count + 1);
+        
+        //masih bernilai 0 ketika di click
         console.log(count);
     }
 
