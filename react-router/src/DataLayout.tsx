2d9613af49201454f2456c7cb4b08a1901d9ea1f
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-09 16:33:51 +0700

    Navigation

--- react-router/src/DataLayout.tsx
+++ react-router/src/DataLayout.tsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router"
+import { NavLink } from "react-router"
+import "./data.css";
 
 export default function DataLayout() {
     return (
         <>
             <div>
                 <h1>Header</h1>
+                <ul>
+                    <li>
+                        <NavLink to={{
+                            pathname: '/data/products',
+                            search: 'category=shoes',
+                            hash: '#top'
+                        }}>
+                            Products
+                        </NavLink>
+                    </li>
+                    <li><NavLink to='/data/sellers'>Sellers</NavLink></li>
+                    <li><NavLink to='/data/customers'>Customers</NavLink></li>
+                </ul>
             </div>
             <div>
                 <Outlet />
