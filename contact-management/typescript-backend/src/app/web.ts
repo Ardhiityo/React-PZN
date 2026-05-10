48fe8f4a995a794d29b2d731cb474b7a0f3c08fb
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-10 15:55:25 +0700

    Setup Backend Project

--- contact-management/typescript-backend/src/app/web.ts
+++ contact-management/typescript-backend/src/app/web.ts
@@ -2,10 +2,12 @@ import express from "express";
 import exception from "../middleware/exception-middleware";
 import publicApi from "../routes/public-api";
 import authApi from "../routes/api";
+import cors from "cors";
 
 const web = express();
 
 web.use(express.json());
+web.use(cors());
 
 web.use(publicApi);
 web.use(authApi);
