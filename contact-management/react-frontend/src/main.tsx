3d80dfaf008ecef827d9150931ca4ec496e62912
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-05-10 22:30:56 +0700

    Dashboard Page

--- contact-management/react-frontend/src/main.tsx
+++ contact-management/react-frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import AuthLayout from './components/AuthLayout'
 import UserRegister from './components/User/UserRegister'
 import UserLogin from './components/User/UserLogin'
+import DashboardLayout from './components/DashboardLayout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,6 +13,9 @@ createRoot(document.getElementById('root')!).render(
         <Route element={<AuthLayout />}>
           <Route path='/register' element={<UserRegister />} />
           <Route path='/login' element={<UserLogin />} />
+        </Route>
+        <Route path='/dashboard' element={<DashboardLayout />}>
+          <Route path='contacts' element={<></>} />
         </Route>
       </Routes>
     </BrowserRouter>
